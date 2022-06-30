e6ccca65d18ec76ca6d439203044f49827d7da2a
Hasan N <hsnnkb.dev@gmail.com>
2022-06-30 13:17:38 +0100

    Iteration 2 -  Add basic operation functionality
    Add 'add', 'subtract', 'divide', 'multiply' and 'operate'
    functions to index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    a = Number(a);
+    b = Number(b);
+    
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null
+            else return divide(a, b);
+        default: 
+            return  null
+    }
+}
